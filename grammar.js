f2662dd0265e50995e0f53f1d3bc2f7a16226f40
fachammer <fabian.achammer@gmail.com>
2019-09-14 14:53:12 +0200

    add ints

--- grammar.js
+++ grammar.js
@@ -3,10 +3,11 @@ module.exports = grammar({
 
   rules: {
     source_file: $ => repeat($._expression),
-    _expression: $ => choice($._comment),
+    _expression: $ => choice($._comment, $.int),
     _comment: $ => choice($.inline_comment, $.block_comment),
     inline_comment: _ => seq('##', repeat(/./),),
     block_comment: $ => seq('#(', repeat(choice(/./, $.block_comment)), ')'),
+    int: $ => /[+-]?[0-9]+/
   },
 
   extras: $ => ['\n']
