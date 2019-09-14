ee9e8ec620aa1d1fb08a249c9439e67887095469
fachammer <fabian.achammer@gmail.com>
2019-09-14 20:31:31 +0200

    add support for symbols and tags

--- grammar.js
+++ grammar.js
@@ -3,7 +3,7 @@ module.exports = grammar({
 
   rules: {
     source_file: $ => repeat($._expression),
-    _expression: $ => choice($._comment, $._number, $.bool),
+    _expression: $ => choice($._comment, $._number, $.bool, $.tag, $.symbol),
     _comment: $ => choice($.inline_comment, $.block_comment),
     inline_comment: _ => seq('##', repeat(/./),),
     block_comment: $ => seq('#(', repeat(choice(/./, $.block_comment)), ')'),
@@ -23,7 +23,11 @@ module.exports = grammar({
           seq($._decimal_part, optional($._exponential_suffix)),
           seq($._natural, $._exponential_suffix),
           seq($._natural, $._decimal_part, optional($._exponential_suffix)))),
-    bool: _ => /true|false/
+    bool: _ => /true|false/,
+    _symbol_start: _ => /[^#\(\)\[\]\{\}0-9\s]/,
+    _symbol: $ => prec.right(seq($._symbol_start, repeat(choice($._symbol_start, $._digit)))),
+    symbol: $ => $._symbol,
+    tag: $ => seq('#', $._symbol)
   },
 
   extras: _ => [/\s/],
