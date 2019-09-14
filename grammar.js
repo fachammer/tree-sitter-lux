8e529fb0b027711a1b9186e5d3181d5db949af4c
fachammer <fabian.achammer@gmail.com>
2019-09-14 21:00:11 +0200

    add lists
    also fixes problems with symbols taking up whitespace

--- grammar.js
+++ grammar.js
@@ -2,11 +2,11 @@ module.exports = grammar({
   name: 'lux',
 
   rules: {
-    source_file: $ => repeat($._expression),
-    _expression: $ => choice($._comment, $._number, $.bool, $.tag, $.symbol),
+    source_file: $ => repeat($._white_spaced_expression),
+    _expression: $ => choice($._comment, $._number, $.bool, $.tag, $.symbol, $.list),
     _comment: $ => choice($.inline_comment, $.block_comment),
     inline_comment: _ => seq('##', repeat(/./),),
-    block_comment: $ => seq('#(', repeat(choice(/./, $.block_comment)), ')'),
+    block_comment: $ => seq('#(', repeat(choice(/./, $._end_line, $.block_comment)), ')'),
     _number: $ => choice($.int, $.real),
     _sign: _ => /[+-]/,
     _digit: _ => /[0-9]/,
@@ -27,8 +27,16 @@ module.exports = grammar({
     _symbol_start: _ => /[^#\(\)\[\]\{\}0-9\s]/,
     _symbol: $ => prec.right(seq($._symbol_start, repeat(choice($._symbol_start, $._digit)))),
     symbol: $ => $._symbol,
-    tag: $ => seq('#', $._symbol)
+    tag: $ => seq('#', $._symbol),
+    _white_space: $ => prec.right(repeat1(choice($._end_line, /\s/))),
+    _white_spaced_expression: $ => prec.right(
+      seq(
+        optional($._white_space), 
+        repeat1($._expression), 
+        optional($._white_space))),
+    list: $ => seq('(', repeat($._white_spaced_expression), ')'),
+    _end_line: _ => /[\r\n]|\r\n/
   },
 
-  extras: _ => [/\s/],
+  extras: $ => [],
 });
