3d8cf924159b0d19f338f594cebe25174ead6790
fachammer <fabian.achammer@gmail.com>
2019-09-14 14:47:56 +0200

    implement comments

--- grammar.js
+++ grammar.js
@@ -2,6 +2,12 @@ module.exports = grammar({
   name: 'lux',
 
   rules: {
-    source_file: $ => 'hello'
-  }
+    source_file: $ => repeat($._expression),
+    _expression: $ => choice($._comment),
+    _comment: $ => choice($.inline_comment, $.block_comment),
+    inline_comment: _ => seq('##', repeat(/./),),
+    block_comment: $ => seq('#(', repeat(choice(/./, $.block_comment)), ')'),
+  },
+
+  extras: $ => ['\n']
 });
