c9493b7be7779f4d6ed0e6c7a87c7a8847ea5967
fachammer <fabian.achammer@gmail.com>
2019-09-14 22:21:38 +0200

    fix formatting

--- grammar.js
+++ grammar.js
@@ -2,20 +2,21 @@ module.exports = grammar({
   name: 'lux'
 
 , rules: {
-    source_file: $ => repeat($._white_spaced_expression,)
-  , _expression: $ => choice(
-      $._comment 
-    , $._number 
-    , $.bool
-    , $.tag 
-    , $.symbol
-    , $.string
-    , $.list
-    , $.tuple
-    , $.record),
-    _comment: $ => choice($.inline_comment, $.block_comment)
+    source_file: $ => repeat($._white_spaced_expression)
+  , _expression: $ =>
+    choice($._comment
+      , $._number
+      , $.bool
+      , $.tag
+      , $.symbol
+      , $.string
+      , $.list
+      , $.tuple
+      , $.record)
+  , _comment: $ => choice($.inline_comment, $.block_comment)
   , inline_comment: _ => seq('##', repeat(/./))
-  , block_comment: $ => seq('#(', repeat(choice(/./, $._end_line, $.block_comment)), ')')
+  , block_comment: $ =>
+    seq('#(', repeat(choice(/./, $._end_line, $.block_comment)), ')')
   , _number: $ => choice($.int, $.real)
   , _sign: _ => /[+-]/
   , _digit: _ => /[0-9]/
@@ -26,23 +27,25 @@ module.exports = grammar({
   , _exponential_suffix: $ => seq($._exponential_delimiter, $._int)
   , _decimal_part: $ => prec.right(seq(/\./, $._natural))
   , real: $ =>
-      seq(
-        optional($._sign)
+    seq(
+      optional($._sign)
       , choice(
-          seq($._decimal_part, optional($._exponential_suffix))
+        seq($._decimal_part, optional($._exponential_suffix))
         , seq($._natural, $._exponential_suffix)
         , seq($._natural, $._decimal_part, optional($._exponential_suffix))))
   , bool: _ => /true|false/
   , _symbol_start: _ => /[^#\(\)\[\]\{\}0-9\s"]/
-  , _symbol: $ => prec.right(seq($._symbol_start, repeat(choice($._symbol_start, $._digit))))
+  , _symbol: $ =>
+    prec.right(seq($._symbol_start, repeat(choice($._symbol_start, $._digit))))
   , symbol: $ => $._symbol
   , tag: $ => seq('#', $._symbol)
   , _white_space: $ => prec.right(repeat1(choice($._end_line, /\s/)))
-  , _white_spaced_expression: $ => prec.right(
+  , _white_spaced_expression: $ =>
+    prec.right(
       seq(
         optional($._white_space)
-      , repeat1($._expression)
-      , optional($._white_space)))
+        , repeat1($._expression)
+        , optional($._white_space)))
   , list: $ => seq('(', repeat($._white_spaced_expression), ')')
   , _end_line: _ => /[\r\n]|\r\n/
   , string: _ => seq('"', repeat(/[^"]/), '"')
