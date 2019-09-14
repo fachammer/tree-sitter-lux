ef19322ba2932218af25935bfd1a85305edf3359
fachammer <fabian.achammer@gmail.com>
2019-09-14 21:08:38 +0200

    add strings

--- grammar.js
+++ grammar.js
@@ -3,7 +3,14 @@ module.exports = grammar({
 
   rules: {
     source_file: $ => repeat($._white_spaced_expression),
-    _expression: $ => choice($._comment, $._number, $.bool, $.tag, $.symbol, $.list),
+    _expression: $ => choice(
+      $._comment, 
+      $._number, 
+      $.bool, 
+      $.tag, 
+      $.symbol, 
+      $.string,
+      $.list),
     _comment: $ => choice($.inline_comment, $.block_comment),
     inline_comment: _ => seq('##', repeat(/./),),
     block_comment: $ => seq('#(', repeat(choice(/./, $._end_line, $.block_comment)), ')'),
@@ -24,7 +31,7 @@ module.exports = grammar({
           seq($._natural, $._exponential_suffix),
           seq($._natural, $._decimal_part, optional($._exponential_suffix)))),
     bool: _ => /true|false/,
-    _symbol_start: _ => /[^#\(\)\[\]\{\}0-9\s]/,
+    _symbol_start: _ => /[^#\(\)\[\]\{\}0-9\s"]/,
     _symbol: $ => prec.right(seq($._symbol_start, repeat(choice($._symbol_start, $._digit)))),
     symbol: $ => $._symbol,
     tag: $ => seq('#', $._symbol),
@@ -35,7 +42,8 @@ module.exports = grammar({
         repeat1($._expression), 
         optional($._white_space))),
     list: $ => seq('(', repeat($._white_spaced_expression), ')'),
-    _end_line: _ => /[\r\n]|\r\n/
+    _end_line: _ => /[\r\n]|\r\n/,
+    string: $ => seq('"', repeat(/[^"]/), '"')
   },
 
   extras: $ => [],
