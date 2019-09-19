073ef174d0e70fcc82a0478f176372549c8ac0ec
fachammer <fabian.achammer@gmail.com>
2019-09-19 19:23:35 +0200

    test: add packaging test
    added sample node project that replicates usage in readme for testing
    correct packaging

---
+++ test/test.js
@@ -0,0 +1,19 @@
+try {
+  const Parser = require('tree-sitter');
+  const Lux = require('tree-sitter-lux');
+
+  const parser = new Parser();
+  parser.setLanguage(Lux);
+
+  const sourceCode = '(+ 1 1)';
+  const tree = parser.parse(sourceCode);
+  const result = tree.rootNode.toString();
+
+
+  const assert = require('assert');
+  assert.equal(result, "(source_file (form (identifier) (nat) (nat)))");
+  console.log("packaging correct!");
+}
+catch (e) {
+  console.error("error in packaging", e);
+}
