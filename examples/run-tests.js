5287779127f41907adc7bd0214d2adf41f830d14
fachammer <fabian.achammer@gmail.com>
2019-09-20 03:52:24 +0200

    perf: pass all files to tree-sitter parse at once
    only get the errors from those files where parsing failed
    therefore we only start separate processes for the files where parsing
    failed

--- examples/run-tests.js
+++ examples/run-tests.js
@@ -3,7 +3,6 @@ const child_process = require('child_process');
 const { promisify } = require('util');
 const exec = promisify(child_process.exec);
 const asyncGlob = promisify(glob);
-const readline = require('readline');
 
 const maxBuffer = 10 * 1024 * 1024;
 const globPattern = 'examples/lux/stdlib/source/**/*.lux';
@@ -13,69 +12,53 @@ function countText(singular, array) {
 }
 
 (async function main() {
+  const files = await asyncGlob(globPattern);
+
   try {
-    const files = await asyncGlob(globPattern);
+    await exec('npx tree-sitter parse --quiet ' + files.join(' '), { maxBuffer });
+    console.log(`successfully parsed ${countText('file', files)}!`)
+    process.exit(0);
+  }
+  catch (error) {
+    if (!error.stdout) {
+      throw error;
+    }
 
-    function pad(index) {
-      return [...Array(files.length.toString().length - index.toString().length).keys()].map(() => ' ').join('') + index;
-    };
-
-    console.log(`parsing ${countText('file', files)}\n`);
+    const errorFiles = error.stdout.split('\n')
+      .filter(line => line)
+      .map(line => line.match(/[^\s]+(?=\s)/)[0]);
 
     const parseErrors = [];
-    for (var index = 0; index < files.length; index++){
-      const file = files[index];
+    for (const file of errorFiles) {
       try {
-        const processingFileString = `\u25CB ${pad(index + 1)} / ${files.length} parsing ${file} ...`;
-        process.stdout.write(processingFileString);
         await exec('npx tree-sitter parse ' + file, { maxBuffer });
-        readline.cursorTo(process.stdout, 0);
-        readline.clearLine(process.stdout, 0);
-        process.stdout.write(`\u2713 ${pad(index + 1)} / ${files.length} parsed ${file}\n`);
       }
-      catch (error) {
-        if (!error.stdout) {
-          throw error;
+      catch (err) {
+        if (!err.stdout) {
+          throw err;
         }
-        var fileErrors = error.stdout.match(/\(ERROR \[(\d+), (\d+)\] - \[(\d+), (\d+)\]\)/g);
+        var fileErrors = err.stdout.match(/\(ERROR \[(\d+), (\d+)\] - \[(\d+), (\d+)\]\)/g);
         fileErrors.pop();
 
-        let errorObject = { file: file, errors: [] };
-        readline.cursorTo(process.stdout, 0);
-        readline.clearLine(process.stdout, 0);
-        process.stdout.write(`\u2715 ${pad(index + 1)} / ${files.length} ${countText('error', fileErrors)} in ${file}\n`);
+        let errorObject = { file, errors: [] };
         for (const fileError of fileErrors) {
           const { start_line, start_character, end_line, end_character } = fileError.match(/\(ERROR \[(?<start_line>\d+), (?<start_character>\d+)\] - \[(?<end_line>\d+), (?<end_character>\d+)\]\)/).groups;
 
           const error = `line${start_line === end_line ? '' : 's'} ${start_line === end_line ? parseInt(start_line) + 1 : start_line + '-' + end_line}, columns ${start_character}-${end_character}`
           errorObject.errors.push(error);
-          console.log(`            ${error}`)
         }
         parseErrors.push(errorObject);
       }
     }
 
-    console.log("\n====================================================\n");
-
-    if (parseErrors.length > 0) {
-      console.log(`parsed ${countText('file', files)}, errors in ${countText('file', parseErrors)} \n`);
-      for (const { file, errors } of parseErrors) {
-        console.log(`${countText('error', errors)} in ${file}`);
-        for (const error of errors) {
-          console.log(`  ${error}`);
-        }
-        console.log();
+    console.log(`parsed ${countText('file', files)}, errors in ${countText('file', parseErrors)} \n`);
+    for (const { file, errors } of parseErrors) {
+      console.log(`${countText('error', errors)} in ${file}`);
+      for (const error of errors) {
+        console.log(`  ${error}`);
       }
-      process.exit(1);
+      console.log();
     }
-    else {
-      console.log(`successfully parsed ${countText('file', files)}!`)
-      process.exit(0);
-    }
-  }
-  catch (e) {
-    console.error(e);
     process.exit(1);
   }
-
 })();
