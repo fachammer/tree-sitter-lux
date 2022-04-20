e9224e1802183ef77ae7895cb7bb26cad13f77d4
Fabian Achammer <fabian@achammer.dev>
2022-04-20 11:23:11 +0200

    test: run parse tests in batches
    to avoid command line limitations on windows

--- examples/run-tests.js
+++ examples/run-tests.js
@@ -1,59 +1,94 @@
-const glob = require('glob');
-const child_process = require('child_process');
-const { promisify } = require('util');
+const glob = require("glob");
+const child_process = require("child_process");
+const { promisify } = require("util");
 const exec = promisify(child_process.exec);
 const asyncGlob = promisify(glob);
 
 const maxBuffer = 10 * 1024 * 1024;
-const globPattern = 'examples/lux/stdlib/source/**/*.lux';
+const globPattern = "examples/lux/stdlib/source/**/*.lux";
+const maxCommandLineLength = 8191;
 
 function countText(singular, array) {
-  return array.length === 1 ? `${array.length} ${singular}` : `${array.length} ${singular}s`;
+  return array.length === 1
+    ? `${array.length} ${singular}`
+    : `${array.length} ${singular}s`;
 }
 
 (async function main() {
   const files = await asyncGlob(globPattern);
+  const filesCopy = [...files];
+
+  const commandPrefix = "npx tree-sitter parse --quiet ";
+
+  const command = (files) => commandPrefix + files.join(" ");
+
+  const fileBatches = [];
+
+  while (files.length > 0) {
+    const fileBatch = [];
+
+    while (
+      command([...fileBatch, files[0]]).length < maxCommandLineLength &&
+      files.length > 0
+    ) {
+      fileBatch.push(files.shift());
+    }
+
+    fileBatches.push(fileBatch);
+  }
 
   try {
-    await exec('npx tree-sitter parse --quiet ' + files.join(' '), { maxBuffer });
-    console.log(`successfully parsed ${countText('file', files)}!`)
+    for (const fileBatch of fileBatches) {
+      await exec(command(fileBatch), {
+        maxBuffer,
+      });
+    }
+
+    console.log(`successfully parsed ${countText("file", filesCopy)}!`);
     process.exit(0);
-  }
-  catch (error) {
+  } catch (error) {
     if (!error.stdout) {
       throw error;
     }
 
-    const errorFiles = error.stdout.split('\n')
-      .filter(line => line)
-      .map(line => line.match(/[^\s]+(?=\s)/)[0]);
+    const errorFiles = error.stdout
+      .split("\n")
+      .filter((line) => line)
+      .map((line) => line.match(/[^\s]+(?=\s)/)[0]);
 
     const parseErrors = [];
     for (const file of errorFiles) {
       try {
-        await exec('npx tree-sitter parse ' + file, { maxBuffer });
-      }
-      catch (err) {
+        await exec("npx tree-sitter parse " + file, { maxBuffer });
+      } catch (err) {
         if (!err.stdout) {
           throw err;
         }
-        var fileErrors = err.stdout.match(/\(ERROR \[(\d+), (\d+)\] - \[(\d+), (\d+)\]\)/g);
-        fileErrors.pop();
+        var fileErrors = err.stdout.match(
+          /\(ERROR \[(\d+), (\d+)\] - \[(\d+), (\d+)\]\)/g
+        );
 
         let errorObject = { file, errors: [] };
         for (const fileError of fileErrors) {
-          const { start_line, start_character, end_line, end_character } = fileError.match(/\(ERROR \[(?<start_line>\d+), (?<start_character>\d+)\] - \[(?<end_line>\d+), (?<end_character>\d+)\]\)/).groups;
+          const { start_line, start_character, end_line, end_character } =
+            fileError.match(
+              /\(ERROR \[(?<start_line>\d+), (?<start_character>\d+)\] - \[(?<end_line>\d+), (?<end_character>\d+)\]\)/
+            ).groups;
 
-          const error = `line${start_line === end_line ? '' : 's'} ${start_line === end_line ? parseInt(start_line) + 1 : start_line + '-' + end_line}, columns ${start_character}-${end_character}`
+          const error = `line${start_line === end_line ? "" : "s"} ${
+            start_line === end_line
+              ? parseInt(start_line) + 1
+              : start_line + "-" + end_line
+          }, columns ${start_character}-${end_character}`;
           errorObject.errors.push(error);
         }
         parseErrors.push(errorObject);
       }
     }
 
-    console.log(`parsed ${countText('file', files)}, errors in ${countText('file', parseErrors)} \n`);
+    console.log(`errors in ${countText("file", parseErrors)} \n`);
     for (const { file, errors } of parseErrors) {
-      console.log(`${countText('error', errors)} in ${file}`);
+      console.log(`${countText("error", errors)} in ${file}`);
       for (const error of errors) {
         console.log(`  ${error}`);
       }
