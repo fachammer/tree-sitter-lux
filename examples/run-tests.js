bdabf2d040551c4721a4283239dc38daee602eb5
Fabian Achammer <fabian@achammer.dev>
2022-04-20 11:40:09 +0200

    test: reduce max command line length further
    previous change didn't seem to be enough

--- examples/run-tests.js
+++ examples/run-tests.js
@@ -6,7 +6,7 @@ const asyncGlob = promisify(glob);
 
 const maxBuffer = 10 * 1024 * 1024;
 const globPattern = "examples/lux/stdlib/source/**/*.lux";
-const maxCommandLineLength = 8000;
+const maxCommandLineLength = 4000;
 
 function countText(singular, array) {
   return array.length === 1
